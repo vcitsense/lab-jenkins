26c71d8d0fc9905b5e349268392b832669c37d63
Victor Cuellar <victor.cuellar@itsense.com.co>
2024-12-01 00:00:21 -0500

    update test.py 4

--- app/test.py
+++ app/test.py
@@ -40,24 +40,15 @@ def test_api(api_url):
         # Realizar la solicitud GET a la API
         response = requests.get(f"{base_url}{api_url}")
         
+        # Crear el nombre del caso de prueba
+        testcase = ET.SubElement(testsuite, "testcase", classname="test", name=f"test_api{api_url}", time="0.001")
+        
         # Add the response details to the testcase
         status_code = str(response.status_code)
         response_text = response.text
         
-        # Crear el nombre del caso de prueba
-        testcase = ET.SubElement(testsuite, "testcase", classname="test", name=f"test_api{api_url}", time="0.001", status=f"{status_code}", response=f"{response_text}")
-        
-        
-        
-        # Check if the response status code is successful (200)
-        if response.status_code == 200:
-            ET.SubElement(testcase, "status", code=status_code, response=response_text)
-        elif response.status_code == 400:
-            ET.SubElement(testcase, "status", code=status_code, response=response_text)
-        elif response.status_code == 500:
-            ET.SubElement(testcase, "status", code=status_code, response=response_text)
-        else:
-            ET.SubElement(testcase, "status", code=status_code, response=response_text)
+        # Add status details to the testcase node
+        status = ET.SubElement(testcase, "status", code=status_code, response=response_text)
         
     except requests.exceptions.RequestException as e:
         testcase = ET.SubElement(testsuite, "testcase", classname="test", name=f"test_api{api_url}", time="0.001")
